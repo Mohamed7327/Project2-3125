6bb6a4c7b69fd01945501ffa6ea2539a10ad78ba
Mohamed7327 <mhajo023@uottawa.ca>
2024-07-22 01:33:02 -0400

    final

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -18,7 +18,7 @@ const ContactUs = () => {
                 </div>
                 <div className="col">
                     <iframe title="map" className="map" width="600px" height="400px" frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22402.113473252903!2d-75.70700106523437!3d45.424175600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce0506f9debc19%3A0xca4076927a053bbd!2s75%20Laurier%20Ave%20E%2C%20Ottawa%2C%20ON%20K1N%206N6!5e0!3m2!1sen!2sca!4v1717956193508!5m2!1sen!2sca" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    <h3>Our Office</h3>
+                    <h3>{t('contactUs.ourOffice')}</h3>
                 </div>
                 <div className="col">
                     <img src={contactUsImg2} alt={t('contactUs.imgAlt2')} className="contactUsImg2" />
--- src/VenueCard.js
+++ src/VenueCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function VenueCard({ venue, onBookNow }) {
+  const { t } = useTranslation();
+
   return (
     <div className="venue-card">
       <img src={venue.image} alt={venue.title} />
       <div className="venue-details">
         <h3>{venue.title}</h3>
-        <p>Size: {venue.size}</p>
-        <p>Price: {venue.price}</p>
-        <p>Location: {venue.location}</p>
-        <button onClick={() => onBookNow(venue)}>Book Now</button>
+        <p>{t('venueCard.size')}: {venue.size}</p>
+        <p>{t('venueCard.price')}: {venue.price}</p>
+        <p>{t('venueCard.location')}: {venue.location}</p>
+        <button onClick={() => onBookNow(venue)}>{t('venueCard.bookNow')}</button>
       </div>
     </div>
   );
